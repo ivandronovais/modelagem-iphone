8e0a33cab1fd2255cab61ebca7cc0fe6f113a79a
ivandronovais <ivandronovais@gmail.com>
2025-03-06 01:18:12 -0300

    📚 docs: update DiagramaUMLiPhone

--- src/ReprodutorMusical.java
+++ src/ReprodutorMusical.java
@@ -4,12 +4,43 @@ public class ReprodutorMusical {
     private boolean emReproducao;
 
     public void tocar() {
+        System.out.println("Tocando " + musicaAtual);
+        emReproducao = true;
     }
 
     public void pausar() {
+        System.out.println("Pausando" + musicaAtual + "...");
+        setEmReproducao(false);
+        setVolume(0);
     }
 
     public void selecionarMusica(String musica) {
     }
+
+    public String getMusicaAtual() {
+        return musicaAtual;
+    }
+
+    public void setMusicaAtual(String musicaAtual) {
+        this.musicaAtual = musicaAtual;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public boolean isEmReproducao() {
+        return emReproducao;
+    }
+
+    public void setEmReproducao(boolean emReproducao) {
+        this.emReproducao = emReproducao;
+    }
+
+    
 
 }
